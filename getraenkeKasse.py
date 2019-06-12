cc77188476d0e91c58da4868b196e2bb9a7345ef
Matthias Roos <matthias.roos@freenet.de>
2019-06-12 22:52:55 +0200

    [getraenkeKasse] added ScanFrame

--- getraenkeKasse.py
+++ getraenkeKasse.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 import wx
 import time
+import barcodescanner as basc
 
 usersFile = "user.txt"
 
-class OrderFrame(wx.Frame):
+class UserFrame(wx.Frame):
 
     def __init__(self):
         """Constructor"""
-        wx.Frame.__init__(self, None, title="Test Fullscreen")
+        wx.Frame.__init__(self, None, title = "Test Fullscreen")
         panel = wx.Panel(self)
 
         width = wx.SystemSettings.GetMetric(wx.SYS_SCREEN_X)
@@ -37,15 +38,15 @@ class OrderFrame(wx.Frame):
         self.SetBackgroundColour("Gray")
 
         #btnBack = wx.Button(panel, id = wx.ID_ANY, label = "back", name = "back", size = wx.Size(btnWidth, btnHeight), pos = (width-2*btnWidth, height-btnHeight))
-        self.btnBack = wx.Button(panel, id = wx.ID_ANY, label = "back", name = "back", size = wx.Size(btnWidth, btnHeight), pos = (0, 0))
-        self.btnBack.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
-        self.btnBack.Bind(wx.EVT_BUTTON,self.OnClickedBackButton)
-        self.btnBack.Disable()
+        #self.btnBack = wx.Button(panel, id = wx.ID_ANY, label = "back", name = "back", size = wx.Size(btnWidth, btnHeight), pos = (0, 0))
+        #self.btnBack.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        #self.btnBack.Bind(wx.EVT_BUTTON,self.OnClickedBackButton)
+        #self.btnBack.Disable()
         #btnConfirm = wx.Button(panel, id = wx.ID_ANY, label = "confirm", name = "confirm", size = wx.Size(btnWidth, btnHeight), pos = (width-1*btnWidth, height-btnHeight))
-        btnConfirm = wx.Button(panel, id = wx.ID_ANY, label = "confirm", name = "confirm", size = wx.Size(btnWidth, btnHeight), pos = ( 100, 100))
-        btnConfirm.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
-        btnConfirm.Bind(wx.EVT_BUTTON,self.OnClickedConfirmButton)
-        btnConfirm.Disable()
+        #self.btnConfirm = wx.Button(panel, id = wx.ID_ANY, label = "confirm", name = "confirm", size = wx.Size(btnWidth, btnHeight), pos = ( 100, 100))
+        #self.btnConfirm.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        #self.btnConfirm.Bind(wx.EVT_BUTTON,self.OnClickedConfirmButton)
+        #self.btnConfirm.Disable()
         self.ShowFullScreen(True)        
     
     def readUsers(self):
@@ -70,29 +71,83 @@ class OrderFrame(wx.Frame):
         print "The button you pressed was labeled: " + button_by_id.GetLabel()
         print "The button's name is " + button_by_id.GetName()
        
-        for btn in self.button:
-            if button_by_id.GetLabel() != btn.GetLabel():
-                btn.Disable()
+        #for btn in self.button:
+        #    if button_by_id.GetLabel() != btn.GetLabel():
+        #        btn.Disable()
         #lbl = wx.StaticText(self, -1, style=wx.ALIGN_CENTER, label=button_by_id.GetName())
         #self.update()
         #time.sleep(60)
+        self.user = button_by_id.GetLabel()
+        #self.btnBack.Enable()
+        frameScan = ScanFrame()
+        #while True:
+        #    print(basc.barcode_reader())
 
-        self.btnBack.Enable()
+    #def OnClickedBackButton(self, event): 
+    #    btn = event.GetEventObject().GetLabel() 
+    #    print "Label of pressed button = ",btn
+    #    for btn in self.button:
+    #        btn.Enable()
+    #    self.btnBack.Disable()
 
-    def OnClickedBackButton(self, event): 
-        btn = event.GetEventObject().GetLabel() 
+    #def OnClickedConfirmButton(self, event):
+    #    btn = event.GetEventObject().GetLabel()
+   #    print "Label of pressed button = ", btn
+
+class ScanFrame(wx.Frame):
+    
+    def __init__(self):
+        """Constructor"""
+        wx.Frame.__init__(self, None, title = "ScanFame", style = wx.DEFAULT_FRAME_STYLE | wx.STAY_ON_TOP)
+        panel = wx.Panel(self)
+
+        width = wx.SystemSettings.GetMetric(wx.SYS_SCREEN_X)
+        height = wx.SystemSettings.GetMetric(wx.SYS_SCREEN_Y)
+
+        btnHeight = 50
+        btnWidth = 150
+
+        #self.SetBackgroundColour("Gray")
+
+        self.btnBack = wx.Button(panel, id = wx.ID_ANY, label = "back", name = "back", size = wx.Size(btnWidth, btnHeight), pos = (width-2*btnWidth, height-btnHeight))
+        #self.btnBack = wx.Button(panel, id = wx.ID_ANY, label = "back", name = "back", size = wx.Size(btnWidth, btnHeight), pos = (0, 0))
+        self.btnBack.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.btnBack.Bind(wx.EVT_BUTTON,self.OnClickedBackButton)
+        #self.btnBack.Disable()
+        self.btnConfirm = wx.Button(panel, id = wx.ID_ANY, label = "confirm", name = "confirm", size = wx.Size(btnWidth, btnHeight), pos = (width-1*btnWidth, height-btnHeight))
+        #self.btnConfirm = wx.Button(panel, id = wx.ID_ANY, label = "confirm", name = "confirm", size = wx.Size(btnWidth, btnHeight), pos = ( 100, 100))
+        self.btnConfirm.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.btnConfirm.Bind(wx.EVT_BUTTON,self.OnClickedConfirmButton)
+        self.btnConfirm.Disable()
+        
+        self.Text = wx.StaticText(panel, label = (frame.user+", was darf es sein?"), pos = (width/3, height/3), size = (150, 50))
+        self.Text.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.Code = wx.TextCtrl(panel, pos = (width/3, height/2), size = (300, 120))
+        self.Code.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+
+
+        self.ShowFullScreen(True)
+
+    def OnClickedBackButton(self, event):
+        """"""
+        btn = event.GetEventObject().GetLabel()
         print "Label of pressed button = ",btn
-        for btn in self.button:
-            btn.Enable()
-        self.btnBack.Disable()
+        #for btn in self.button:
+        #    btn.Enable()
+        #self.btnBack.Disable()
+        self.Close()
 
     def OnClickedConfirmButton(self, event):
+        """"""
         btn = event.GetEventObject().GetLabel()
         print "Label of pressed button = ", btn
 
 
+
+
+
 if __name__ == "__main__":
     app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
-    frame = OrderFrame()
+    frame = UserFrame()
     app.MainLoop()
 
