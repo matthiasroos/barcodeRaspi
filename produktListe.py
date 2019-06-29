69666ccbc72650f89e0dc8aeaef8dfa9c9983b34
Matthias Roos <matthias.roos@freenet.de>
2019-06-29 18:18:02 +0200

    [produktListe] change of font size + column with price

--- produktListe.py
+++ produktListe.py
@@ -6,6 +6,7 @@ import time
 productlist = "produt.txt"
 btnHeight = 50
 btnWidth = 150
+fontsize = 14
 
 
 class ListFrame(wx.Frame):
@@ -25,27 +26,28 @@ class ListFrame(wx.Frame):
 
         btnClose = wx.Button(panel, id = wx.ID_ANY, label = "close", name = "close", size = wx.Size(btnWidth, btnHeight), pos = (type(self).width-btnWidth, 0))
         #btnClose = wx.Button(panel, id = wx.ID_ANY, label = "close", name = "close", size = wx.Size(btnWidth, btnHeight), pos = ((type(self).width/2)-btnWidth, 0))
-        btnClose.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        btnClose.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
         btnClose.Bind(wx.EVT_LEFT_UP,self.onClickCloseButton)
 
         btnAdd = wx.Button(panel, id = wx.ID_ANY, label = "add", name = "add", size = wx.Size(btnWidth, btnHeight), pos = (type(self).width-btnWidth, 2*btnHeight))
-        btnAdd.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        btnAdd.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
         btnAdd.Bind(wx.EVT_LEFT_UP, self.onClickAddButton)
 
         btnEdit = wx.Button(panel, id = wx.ID_ANY, label = "edit", name = "edit", size = wx.Size(btnWidth, btnHeight), pos = (type(self).width-btnWidth, 3*btnHeight))
-        btnEdit.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        btnEdit.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
 
         btnDel = wx.Button(panel, id = wx.ID_ANY, label = "delete", name = "delete", size = wx.Size(btnWidth, btnHeight), pos = (type(self).width-btnWidth, 4*btnHeight))
-        btnDel.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        btnDel.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
 
         btnSave = wx.Button(panel, id = wx.ID_ANY, label = "save", name = "save", size = wx.Size(btnWidth, btnHeight), pos = (type(self).width-btnWidth, 5*btnHeight))
-        btnSave.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        btnSave.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
 
         self.prodList = wx.ListCtrl(panel, size=((type(self).width-btnWidth-2*offset), type(self).height-2*offset), pos = (offset, offset), style = wx.LC_REPORT|wx.LC_HRULES)
-        self.prodList.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.prodList.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
         self.prodList.InsertColumn(0, '#', width = 50)
-        self.prodList.InsertColumn(1, 'code', width = 200)
-        self.prodList.InsertColumn(2, 'description', width = 350)
+        self.prodList.InsertColumn(1, 'code', width = 250)
+        self.prodList.InsertColumn(2, 'description', width = 250)
+        self.prodList.InsertColumn(3, 'price', width = 70)
 
         self.SetBackgroundColour("Gray")
         self.ShowFullScreen(True)        
