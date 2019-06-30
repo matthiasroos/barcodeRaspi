7bf15946747b89a2db03007319ebb6630c416577
Matthias Roos <matthias.roos@freenet.de>
2019-06-30 08:12:48 +0200

    [produktListe/EditFrame] text + input fields

--- produktListe.py
+++ produktListe.py
@@ -80,25 +80,45 @@ class ListFrame(wx.Frame):
 
 class EditFrame(wx.Frame):
 
+    frameWidth = 500
+    frameHeight = 300
+
     def __init__(self):
         """Constructor"""
-        wx.Frame.__init__(self, None, title = "Add/Edit entry", size = (500, 500))
+        wx.Frame.__init__(self, None, title = "Add/Edit entry", size = (type(self).frameWidth, type(self).frameHeight))
 
-        btnBack = wx.Button(self, id = wx.ID_ANY, label = "back", name = "back", size = wx.Size(btnWidth, btnHeight), pos = (ListFrame.width-2*btnWidth, ListFrame.height-btnHeight))
-        btnBack.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.number = frame.prodList.GetItemCount()
+        self.NummerTxt = wx.StaticText(self, label = ("#"+str(self.number+1)), pos = (40, 20), size = (20, 50))
+        self.NummerTxt.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
+
+        CodeTxt = wx.StaticText(self, label = ("code"), pos = (40, 70), size = (20, 50))
+        CodeTxt.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.CodeInp = wx.TextCtrl(self, pos = (140, 60), size = (300, 50))
+        self.CodeInp.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.CodeInp.SetMaxLength(13)
+        self.CodeInp.SetFocus()
+
+        DescTxt = wx.StaticText(self, label = ("descr."), pos = (40, 125), size = (20, 50))
+        DescTxt.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.DescInp = wx.TextCtrl(self, pos = (140, 115), size = (300, 50))
+        self.DescInp.SetMaxLength(20)
+        self.DescInp.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
+
+        PriceTxt = wx.StaticText(self, label = ("price"), pos = (40, 180), size = (20, 50))
+        PriceTxt.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.PriceInp = wx.TextCtrl(self, pos = (140, 170), size = (300, 50))
+        self.PriceInp.SetMaxLength(6)
+        self.PriceInp.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
+
+
+        btnBack = wx.Button(self, id = wx.ID_ANY, label = "back", name = "back", size = wx.Size(btnWidth, btnHeight), pos = (60, type(self).frameHeight-2*btnHeight))
+        btnBack.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
         #btnBack.Bind(wx.EVT_LEFT_UP,self.onClickBackButton)
         
-        btnConfirm = wx.Button(self, id = wx.ID_ANY, label = "confirm", name = "confirm", size = wx.Size(btnWidth, btnHeight), pos = (ListFrame.width-1*btnWidth, ListFrame.height-btnHeight))
-        btnConfirm.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
-        #btnConfirm.Bind(wx.EVT_LEFT_UP,self.onClickConfirmButton)
-        btnConfirm.Disable()
-
-        Text = wx.StaticText(self, label = ("was darf es sein?"), pos = (ListFrame.width/3, ListFrame.height/3), size = (150, 50))
-        Text.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
-        Code = wx.TextCtrl(self, pos = (ListFrame.width/3, ListFrame.height/2), size = (300, 120))
-        Code.SetFont(wx.Font(20, wx.SWISS, wx.NORMAL, wx.BOLD))
-        Code.SetFocus()
-
+        btnConfirm = wx.Button(self, id = wx.ID_ANY, label = "confirm", name = "confirm", size = wx.Size(btnWidth, btnHeight), pos = (60+btnWidth+5, type(self).frameHeight-2*btnHeight))
+        btnConfirm.SetFont(wx.Font(fontsize, wx.SWISS, wx.NORMAL, wx.BOLD))
+        btnConfirm.Bind(wx.EVT_LEFT_UP,self.onClickConfirmButton)
+        #btnConfirm.Disable()
 
         self.Show()
 
