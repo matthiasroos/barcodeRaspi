11223a0e46ad6ba47f53742d99cb75c3ac43aa20
Matthias Roos <matthias.roos@freenet.de>
2020-05-09 14:14:57 +0200

    add further wx mocks

--- tests/test_getraenkeapp.py
+++ tests/test_getraenkeapp.py
@@ -11,15 +11,17 @@ import getraenkeapp
 
 @pytest.fixture
 def mock_getraenkekasse():
-    unittest.mock.patch('wx.App', autospec=True)
-    gk = getraenkeapp.GetraenkeApp()
-    products = [[1, '1111111111111', 'xxxx', 0.60, 20], [2, '2222222222222', 'yyyy', 0.80, 0]]
-    gk.fileContents.products = pd.DataFrame(products, columns=['nr', 'code', 'desc', 'price', 'stock'])
-    purchases = [['2019-12-10T12:20:00', 'aaa', '111111111111', False],
-                 ['2019-12-10T16:30:00', 'bbb', '222222222222', False],
-                 ['2019-12-10T16:35:00', 'bbb', '222222222222', False]]
-    gk.fileContents.purchases = pd.DataFrame(purchases, columns=['timestamp', 'user', 'code', 'paid'])
-    gk.fileContents.users = None
+    with unittest.mock.patch('wx.App', autospec=True), \
+         unittest.mock.patch('wx.SystemSettings', autospec=True), \
+         unittest.mock.patch('wx.Font', autospec=True):
+        gk = getraenkeapp.GetraenkeApp()
+        products = [[1, '1111111111111', 'xxxx', 0.60, 20], [2, '2222222222222', 'yyyy', 0.80, 0]]
+        gk.fileContents.products = pd.DataFrame(products, columns=['nr', 'code', 'desc', 'price', 'stock'])
+        purchases = [['2019-12-10T12:20:00', 'aaa', '111111111111', False],
+                     ['2019-12-10T16:30:00', 'bbb', '222222222222', False],
+                     ['2019-12-10T16:35:00', 'bbb', '222222222222', False]]
+        gk.fileContents.purchases = pd.DataFrame(purchases, columns=['timestamp', 'user', 'code', 'paid'])
+        gk.fileContents.users = None
     return gk
 
 
