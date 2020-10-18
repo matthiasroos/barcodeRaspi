9eca41836d4a97a973d63cb76fcc02c38d5464b5
Matthias Roos <matthias.roos@freenet.de>
2020-10-18 20:56:40 +0200

    Use correct namings, make variable names pythonic, use central wx.Size object

--- src/getraenkeKasse/scanframe.py
+++ src/getraenkeKasse/scanframe.py
@@ -19,54 +19,57 @@ class ScanFrame(wx.Frame):
 
         with self.parent as prt:
             prt.load_products()
-            self.btnNoCode = wx.Button(self.panel, id=wx.ID_ANY, label="no barcode", name="no barcode",
-                                       size=wx.Size(prt.displaySettings.btnWidth, prt.displaySettings.btnHeight),
-                                       pos=(prt.displaySettings.screen_width - 3*prt.displaySettings.btnWidth,
-                                            prt.displaySettings.screen_height - prt.displaySettings.btnHeight))
-            self.btnNoCode.SetFont(prt.displaySettings.wxFont)
-            self.btnNoCode.Bind(wx.EVT_LEFT_UP, self._onClickNoCodeButton)
-            self.btnNoCode.Disable()
-            self.btnBack = wx.Button(self.panel, id=wx.ID_ANY, label="back", name="back",
-                                     size=wx.Size(prt.displaySettings.btnWidth, prt.displaySettings.btnHeight),
-                                     pos=(prt.displaySettings.screen_width - 2*prt.displaySettings.btnWidth,
-                                          prt.displaySettings.screen_height - prt.displaySettings.btnHeight))
-            self.btnBack.SetFont(prt.displaySettings.wxFont)
-            self.btnBack.Bind(wx.EVT_LEFT_UP, self._onClickBackButton)
-            self.btnConfirm = wx.Button(self.panel, id=wx.ID_ANY, label="confirm", name="confirm",
-                                        size=wx.Size(prt.displaySettings.btnWidth, prt.displaySettings.btnHeight),
-                                        pos=(prt.displaySettings.screen_width - 1*prt.displaySettings.btnWidth,
-                                             prt.displaySettings.screen_height - prt.displaySettings.btnHeight))
-            self.btnConfirm.SetFont(prt.displaySettings.wxFont)
-            self.btnConfirm.Bind(wx.EVT_LEFT_UP, self._onClickConfirmButton)
-            self.btnConfirm.Disable()
+            self.btn_nocode = wx.Button(self.panel, id=wx.ID_ANY, label="no barcode", name="no barcode",
+                                        size=prt.display_settings.wx_button_size,
+                                        pos=(prt.display_settings.screen_width - 3 * prt.display_settings.btn_width,
+                                             prt.display_settings.screen_height - prt.display_settings.btn_height))
+            self.btn_nocode.SetFont(prt.display_settings.wx_font)
+            self.btn_nocode.Bind(wx.EVT_LEFT_UP, self._onClickNoCodeButton)
+            self.btn_nocode.Disable()
+
+            self.btn_back = wx.Button(self.panel, id=wx.ID_ANY, label="back", name="back",
+                                      size=prt.display_settings.wx_button_size,
+                                      pos=(prt.display_settings.screen_width - 2 * prt.display_settings.btn_width,
+                                           prt.display_settings.screen_height - prt.display_settings.btn_height))
+            self.btn_back.SetFont(prt.display_settings.wx_font)
+            self.btn_back.Bind(wx.EVT_LEFT_UP, self._onClickBackButton)
+
+            self.btn_confirm = wx.Button(self.panel, id=wx.ID_ANY, label="confirm", name="confirm",
+                                         size=prt.display_settings.wx_button_size,
+                                         pos=(prt.display_settings.screen_width - 1 * prt.display_settings.btn_width,
+                                              prt.display_settings.screen_height - prt.display_settings.btn_height))
+            self.btn_confirm.SetFont(prt.display_settings.wx_font)
+            self.btn_confirm.Bind(wx.EVT_LEFT_UP, self._onClickConfirmButton)
+            self.btn_confirm.Disable()
 
             self.Text = wx.StaticText(self.panel, label=(prt.clicked_user + ", what can I get you?"),
-                                      pos=(prt.displaySettings.screen_width/5, prt.displaySettings.screen_height*1/5),
+                                      pos=(prt.display_settings.screen_width / 5,
+                                           prt.display_settings.screen_height * 1 / 5),
                                       size=(150, 50))
-            self.Text.SetFont(prt.displaySettings.wxFont)
+            self.Text.SetFont(prt.display_settings.wx_font)
 
-            self.Code = wx.TextCtrl(self.panel, pos=(prt.displaySettings.screen_width/5,
-                                                     prt.displaySettings.screen_height*1/5 + 70),
+            self.Code = wx.TextCtrl(self.panel, pos=(prt.display_settings.screen_width / 5,
+                                                     prt.display_settings.screen_height * 1 / 5 + 70),
                                     size=(320, 50))
-            self.Code.SetFont(prt.displaySettings.wxFont)
+            self.Code.SetFont(prt.display_settings.wx_font)
             self.Code.SetMaxLength(13)
             self.Code.SetFocus()
             self.Code.Bind(wx.EVT_TEXT, self._onChangeCode)
 
-            self.Product = wx.StaticText(self.panel, label="", pos=(prt.displaySettings.screen_width/5,
-                                                                    prt.displaySettings.screen_height*1/5 + 150),
+            self.Product = wx.StaticText(self.panel, label="", pos=(prt.display_settings.screen_width/5,
+                                                                    prt.display_settings.screen_height*1/5 + 150),
                                          size=(150, 50))
-            self.Product.SetFont(prt.displaySettings.wxFont)
+            self.Product.SetFont(prt.display_settings.wx_font)
 
         self.ShowFullScreen(True)
 
     def _onClickNoCodeButton(self, _) -> None:
         """"""
-        self.btnNoCode.Disable()
+        self.btn_nocode.Disable()
         self.Code.Hide()
         self.cmbProducts = wx.ComboBox(self.panel, id=wx.ID_ANY,
-                                       pos=(self.parent.displaySettings.screen_width / 5,
-                                            self.parent.displaySettings.screen_height * 1 / 5 + 70),
+                                       pos=(self.parent.display_settings.screen_width / 5,
+                                            self.parent.display_settings.screen_height * 1 / 5 + 70),
                                        size=(320, 50))
         nrProducts = len(self.parent.products)
 
@@ -92,7 +95,7 @@ class ScanFrame(wx.Frame):
                 ind = select_df.first_valid_index()
                 self.Product.SetLabel(label=f"{str(select_df.at[ind, 'desc'])}\t Price: " +
                                       "{:.2f}".format(select_df.at[ind, 'price']))
-                self.btnConfirm.Enable()
+                self.btn_confirm.Enable()
                 return None
         self.Product.SetLabel("")
-        self.btnConfirm.Disable()
+        self.btn_confirm.Disable()
